dc9620d35ef1937f3d68785fdd9f1751426d1824
wemersonprocopio <120763216+wemersonprocopio@users.noreply.github.com>
2026-04-05 17:02:23 -0300

    Add SQL query to retrieve sales data with joins

--- sql/query_vendas.sql
+++ sql/query_vendas.sql
@@ -1,1 +1,12 @@
-
+SELECT 
+    s.SalesKey,
+    s.OrderDate,
+    s.SalesAmount,
+    s.Quantity,
+    p.ProductName,
+    c.CustomerName,
+    st.StoreName
+FROM FactSales s
+LEFT JOIN DimProduct p ON s.ProductKey = p.ProductKey
+LEFT JOIN DimCustomer c ON s.CustomerKey = c.CustomerKey
+LEFT JOIN DimStore st ON s.StoreKey = st.StoreKey;
